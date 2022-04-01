d8060d8f108ed253a2a3ad7e8554e696190bbc4e
pau7rr <pau.rosquellas@gmail.com>
2022-04-01 22:41:07 +0200

    bgColor, textColor, borderColor via optional prop

--- src/vue-profile-avatar.vue
+++ src/vue-profile-avatar.vue
@@ -6,47 +6,32 @@ export default defineComponent({
   props: {
     username: { type: String, required: true },
     size: { type: String, default: 's', required: false },
-    border: { type: Boolean, default: true, required: false }
+    border: { type: Boolean, default: true, required: false },
+    bgColor: { type: String, required: false },
+    borderColor: { type: String, required: false },
+    textColor: { type: String, required: false },
   },
   data() {
     return {
        arrBackgroundColors: {
-        a: '#FF6633',
-        b: '#FFB399', 
-        c: '#FF33FF', 
-        d: '#FFFF99', 
-        e: '#00B3E6', 
-        f: '#E6B333',
-        g: '#3366E6', 
-        h: '#999966',
-        i: '#99FF99', 
-        j: '#B34D4D',
-        k: '#80B300',
-        l: '#809900', 
-        m: '#E6B3B3',
-        n: '#6680B3', 
-        o: '#66991A', 
-        p: '#FF99E6', 
-        q: '#CCFF1A', 
-        r: '#FF1A66', 
-        s: '#E6331A',
-        t: '#33FFCC', 
-        u: '#66994D', 
-        v: '#B366CC', 
-        w: '#4D8000', 
-        x: '#B33300', 
-        y: '#CC80CC', 
-        z: '#66664D', 
-        1: '#4D8066', 
-        2: '#809980', 
-        3: '#E6FF80', 
-        4: '#1AFF33', 
-        5: '#999933', 
-        6: '#FF3380', 
-        7: '#CCCC00', 
-        8: '#66E64D', 
-        9: '#4D80CC', 
-        0: '#9900B3', 
+        a: '#FF6633', b: '#FFB399', 
+        c: '#FF33FF', d: '#FFFF99', 
+        e: '#00B3E6', f: '#E6B333',
+        g: '#3366E6', h: '#999966',
+        i: '#99FF99', j: '#B34D4D',
+        k: '#80B300', l: '#809900', 
+        m: '#E6B3B3', n: '#6680B3', 
+        o: '#66991A', p: '#FF99E6', 
+        q: '#CCFF1A', r: '#FF1A66', 
+        s: '#E6331A', t: '#33FFCC', 
+        u: '#66994D', v: '#B366CC', 
+        w: '#4D8000', x: '#B33300', 
+        y: '#CC80CC', z: '#66664D', 
+        1: '#4D8066', 2: '#809980', 
+        3: '#E6FF80', 4: '#1AFF33', 
+        5: '#999933', 6: '#FF3380', 
+        7: '#CCCC00', 8: '#66E64D', 
+        9: '#4D80CC', 0: '#9900B3', 
         else: '#6666FF'
       }
     };
@@ -70,7 +55,7 @@ export default defineComponent({
     },
     backgroundColor() {
       return this.getColorByInitial(this.getInitials(this.username)[0])
-    }
+    },
   },
   methods: {
     getInitials(username) {
@@ -85,10 +70,21 @@ export default defineComponent({
       return initials.join('')
     },
     getColorByInitial(initial) {
+      if (this.bgColor) return this.bgColor
+
       return this.arrBackgroundColors[initial.toLowerCase()]
     },
-    shadeColor(color, percent) {
+    getBorderColor(color, percent) {
+      if (this.borderColorPropExists()) return this.borderColor
 
+      return this.shadeColor(color, percent)
+    },
+    getTextColor(color, percent) {
+      if (this.textColorPropExists()) return this.textColor
+
+      return this.shadeColor(color, percent)
+    },
+    shadeColor(color, percent) {
       var R = parseInt(color.substring(1,3),16);
       var G = parseInt(color.substring(3,5),16);
       var B = parseInt(color.substring(5,7),16);
@@ -106,6 +102,14 @@ export default defineComponent({
       var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
 
       return "#"+RR+GG+BB;
+    },
+    borderColorPropExists() {
+      if (this.borderColor) return true
+      return false
+    },
+    textColorPropExists() {
+      if (this.textColor) return true
+      return false
     }
   },
 });
@@ -117,10 +121,10 @@ export default defineComponent({
     :class="[avatarClass, isBorder]" 
     :style="{ 
       backgroundColor: backgroundColor, 
-      color: shadeColor(backgroundColor, -45), 
-      borderColor:  shadeColor(backgroundColor, -45)  
-      }"
-    >
+      color: getTextColor(backgroundColor, -45), 
+      borderColor: getBorderColor(backgroundColor, -45),
+    }"
+  >
     <span class="text"> {{initials}} </span>
   </div>
 </template>
