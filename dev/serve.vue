d64ad6667d7483af697604058918f631fc399c66
pau7rr <pau.rosquellas@gmail.com>
2022-04-01 16:47:47 +0200

    avatar background color

--- dev/serve.vue
+++ dev/serve.vue
@@ -12,6 +12,6 @@ export default defineComponent({
 
 <template>
   <div id="app">
-    <profile-avatar username="pau7rr" />
+    <profile-avatar username="pau rosquellas" />
   </div>
 </template>
--- src/vue-profile-avatar.vue
+++ src/vue-profile-avatar.vue
@@ -8,6 +8,58 @@ export default defineComponent({
     size: { type: String, default: 's', required: false },
     border: { type: Boolean, default: true, required: false }
   },
+  data() {
+    return {
+       arrBackgroundColors: {
+        a: '#FF6633' ,
+        b: '#FFB399' , 
+        c: '#FF33FF', 
+        d: '#FFFF99', 
+        e: '#00B3E6', 
+        f: '#E6B333',
+        g: '#3366E6', 
+        h: '#999966',
+        i: '#99FF99', 
+        j: '#B34D4D',
+        k: '#80B300',
+        l: '#809900', 
+        m: '#E6B3B3',
+        n: '#6680B3', 
+        o: '#66991A', 
+        p: '#FF99E6', 
+        q: '#CCFF1A', 
+        r: '#FF1A66', 
+        s: '#E6331A',
+        t: '#33FFCC', 
+        u: '#66994D', 
+        v: '#B366CC', 
+        w: '#4D8000', 
+        x: '#B33300', 
+        y: '#CC80CC', 
+        z: '#66664D',
+        '_': '#991AFF',
+        '@': '#E666FF',
+        '!': '#4DB3FF', 
+        '#': '#1AB399', 
+        '-': '#E666B3', 
+        '/': '#33991A', 
+        '=': '#CC9999', 
+        '-': '#B3B31A', 
+        '+': '#00E680', 
+        1: '#4D8066', 
+        2: '#809980', 
+        3: '#E6FF80', 
+        4: '#1AFF33', 
+        5: '#999933', 
+        6: '#FF3380', 
+        7: '#CCCC00', 
+        8: '#66E64D', 
+        9: '#4D80CC', 
+        0: '#9900B3', 
+        else: '#6666FF'
+      }
+    };
+  },
   computed: {
     avatarClass() {
       return {
@@ -22,25 +74,35 @@ export default defineComponent({
       }
     },
     initials() {
+      this.backgroundColor
       return this.getInitials(this.username)
+    },
+    backgroundColor() {
+      return this.getColorByInitial(this.getInitials(this.username)[0])
     }
   },
   methods: {
     getInitials(username) {
+      const usernameParts = username.split(' ')
 
+      const initials = usernameParts.map((e) => {
+        return e.slice(0, 1).toUpperCase()
+      })
+
+      if (initials.length > 3) return initials.slice(0, 3).join('')
+
+      return initials.join('')
+    },
+    getColorByInitial(initial) {
+      return this.arrBackgroundColors[initial.toLowerCase()]
     }
-  },
-  data() {
-    return {
-      
-    };
   },
 });
 </script>
 
 <template>
-  <div class="avatarContainer" :class="[avatarClass, isBorder]">
-    <span class="text"> PR </span>
+  <div class="avatarContainer" :class="[avatarClass, isBorder]" :style="{backgroundColor: backgroundColor}">
+    <span class="text"> {{initials}} </span>
   </div>
 </template>
 
@@ -50,7 +112,7 @@ export default defineComponent({
   .avatarContainer {
     display: flex;
     border-radius: 100%;
-    background-color: cornflowerblue;
+    background-color: #FF6633;
   }
 
   .small {
